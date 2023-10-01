f534bb1bd28ecb7c51c9a60c162c435f031d49b0
Ahmed-Magdy-El-Sayed <109208478+Ahmed-Magdy-El-Sayed@users.noreply.github.com>
2023-10-01 17:00:24 +0300

    update backup

--- controller/sendEmail.js
+++ controller/sendEmail.js
@@ -5,15 +5,15 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     secure: true,
     auth: {
-        user: 'amcodes55@gmail.com',// add your email that will be the sender
-        pass: 'ziichubwyythblop'// add your email app password
+        user: '',// add your email that will be the sender
+        pass: ''// add your email app password
     }
 });
 
 module.exports={
     sendEmail: async function (email, notif) {
         const mailOptions = {
-            from: 'amcodes55@gmail.com',//repeat your email again
+            from: '',//repeat your email again
             to: email,
             subject: notif.title,
             html: `
